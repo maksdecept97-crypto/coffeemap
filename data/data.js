37edbe293995c3e60175330e618110d3158ced36
maksdecept97-crypto <maks.decept97@gmail.com>
2025-09-19 16:24:13 +0300

    Update and rename data.js to data/data.js

---
+++ data/data.js
@@ -0,0 +1,10 @@
+/* ======= Данные кофеен ======= */
+const coffeeData = [
+  {id:1,name:"Tour Coffee",coordinates:[59.911946,30.316499],address:"7-я Красноармейская ул., 5Д",description:"Tour coffee — это предвкушение каждый раз чего-то нового: разные сорта, разные обжарщики. Здесь подают гриль-чизы и свежие десерты. И конечно кофе.",telegramEmbed:"https://t.me/teplo_coffeeroutes/4?embed=1",tags:["center","with-food"]},
+  {id:2,name:"UNIC",coordinates:[59.960012,30.311556],address:"ул. Кронверская, 12",description:"UNIC — место, куда хочется приходить за едой и кофе.",telegramEmbed:"https://t.me/teplo_coffeeroutes/8?embed=1",tags:["center","with-food"]},
+  {id:3,name:"Verlé",coordinates:[59.963493,30.314817],address:"Каменноостровский пр-т, 25/2",description:"Уютное место с отличным кофе и атмосферой.",telegramEmbed:"https://t.me/teplo_coffeeroutes/15?embed=1",tags:["center"]},
+  {id:4,name:"Щегол",coordinates:[59.941899,30.363427],address:"ул. Радищева, 38/20",description:"Уютная кофейня с тёплым, домашним дизайном.",telegramEmbed:"https://t.me/teplo_coffeeroutes/22?embed=1",tags:["center","with-food"]},
+  {id:5,name:"Bolshecoffee roasters",coordinates:[59.927991,30.353581],address:"ул. Марата, 22-2",description:"Bolshecoffee Roasters — место с ароматным кофе и десертами.",telegramEmbed:"",tags:["center","with-food"]},
+  {id:6,name:"Сибаристика",coordinates:[59.910412,30.284159],address:"наб. Обводного канала, 199-201К",description:"Sibaristica — коворкинг и школа бариста с интересным меню.",telegramEmbed:"https://t.me/teplo_coffeeroutes/35",tags:["with-food"]},
+  {id:7,name:"Щегол (2)",coordinates:[59.912521,30.317260],address:"6-я Красноармейская ул., 1",description:"Приятная кофейня с вкусным меню и прекрасным кофе.",telegramEmbed:"https://t.me/teplo_coffeeroutes/22?embed=1",tags:["with-food"]}
+];
